7bb6dde606b13295f4255c1fdc237647312ed46c
heartlesspatidar <npdnitin9@gmail.com>
2025-01-20 11:49:19 +0530

    Deploy to main branch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // Dynamic base path configuration
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/twilio-services/' : '/', 
+  base: process.env.NODE_ENV === 'production' ? '/twilio-client/' : '/', 
   server: {
     port: process.env.VITE_PORT || 5173,
   },
